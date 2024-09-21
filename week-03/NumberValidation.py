7b9537dc96688bcfd17a798066f59cfca3e996c6
oussama hamani <oussamahamani.dev@gmail.com>
2024-09-21 12:11:38 -0400

    finishing first exercise

---
+++ week-03/NumberValidation.py
@@ -0,0 +1,6 @@
+number = input("type a number between 1 and 9: ")
+number = int(number)
+if 1<=number<=9:
+    print(f"{number} is between 1 and 9")
+else:
+    print(f"{number} is not between 1 and 9")
---
+++ week-03/index.py
@@ -0,0 +1,5 @@
+message = input("give me a number: ")
+number = int(message)
+
+print(f"you typed {number}!")
+print(f"Adding 5 to {number} gives {number+5}")
